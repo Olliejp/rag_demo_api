8d55add1601b1c82343960c5d06b5c10ae60acb4
Oliver Paul <ollie@olliep.home>
2025-11-01 14:02:45 +0100

    modified origin traffic

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 # Configure CORS for frontend communication
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://rag-demo-frontend-r20o.onrender.com",
+        "http://localhost:5173"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
